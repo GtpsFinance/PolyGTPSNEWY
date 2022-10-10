0e51b3727bf80fa164dc0cd567da96215e19fdeb
David Gomadza <welcome@gtps.finance>
2022-10-10 05:19:37 +0100

    Initial commit

---
+++ src/constants/multicall/index.ts
@@ -0,0 +1,9 @@
+import { ChainId } from 'sdkv2'
+import MULTICALL_ABI from './abi.json'
+
+const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
+  [ChainId.MAINNET]: '0x11ce4B23bD875D7F5C6a31084f55fDe1e9A87507', // TODO
+  [ChainId.BSCTESTNET]: '0x301907b5835a2d723Fe3e9E8C5Bc5375d5c1236A'
+}
+
+export { MULTICALL_ABI, MULTICALL_NETWORKS }
